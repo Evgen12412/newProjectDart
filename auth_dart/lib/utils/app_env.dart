a7df49099c40646f9eb37ad896488176fd749a17
evgenijs <evgeny.krd2016@yandex.ru>
2024-05-19 09:54:30 +0300

    completed run docker

--- auth_dart/lib/utils/app_env.dart
+++ auth_dart/lib/utils/app_env.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 abstract class AppEnv {
   AppEnv._();
   static final String secretKey =
-      Platform.environment["SECRET_KEY"] ?? "SECRET_KEY";
+      Platform.environment["SECRET_KEY"] ?? "";
 
-  static final String port = Platform.environment["PORT"] ?? "5431";
+  static final String port = Platform.environment["PORT"] ?? "";
 
-  static final dbUsername = Platform.environment["DB_USERNAME"] ?? "root";
-  static final dbPassword = Platform.environment["DB_PASSWORD"] ?? "root";
-  static final dbHost = Platform.environment["DB_HOST"] ?? "localhost";
-  static final dbPort = Platform.environment["DB_PORT"] ?? "6101";
-  static final dbDatabaseName = Platform.environment["DB_NAME"] ?? "postgres";
+  static final dbUsername = Platform.environment["DB_USERNAME"] ?? "";
+  static final dbPassword = Platform.environment["DB_PASSWORD"] ?? "";
+  static final dbHost = Platform.environment["DB_HOST"] ?? "";
+  static final dbPort = Platform.environment["DB_PORT"] ?? "";
+  static final dbDatabaseName = Platform.environment["DB_NAME"] ?? "";
 }
